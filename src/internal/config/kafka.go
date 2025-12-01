05b03e143fc8aa5e8bf0ed5401e9c9c1e90a511e
Farid Tri Wicaksono <farid.wicak@gmail.com>
2025-12-01 16:25:59 +0700

    feat: setup pipeline

--- src/internal/config/kafka.go
+++ src/internal/config/kafka.go
@@ -12,8 +12,10 @@ func NewKafkaConfig(viper *viper.Viper) kafkaPkgConfluent.KafkaConfig {
 		KafkaUrl:      viper.GetString("kafka.bootstrap.servers"),
 		KafkaUsername: viper.GetString("kafka.username"),
 		KafkaPassword: viper.GetString("kafka.password"),
+		KafkaCaCert:   viper.GetString("kafka.cacert"),
 		AppName:       viper.GetString("kafka.group.id"),
 		Offset:        viper.GetString("kafka.auto.offset.reset"),
+
 	}
 	return kafkaPkgConfluent.InitKafkaConfig(configKafka)
 
